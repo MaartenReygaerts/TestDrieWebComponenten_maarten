0886248265613c9862fc977983ba1e3d6486f7df
bartAtHome <hunerbeinbart@gmail.com>
2019-09-17 20:50:28 +0200

    17/20 goede oplossing! wel nog enkele kinderzeertjes hier en daar die je kan wegwerken door wat extra variables aan te maken of je code wat mooier te schrijven.

--- src/main/java/be/intecbrussel/webcomponenten/test/maarten/servlet/TestDrieServlet.java
+++ src/main/java/be/intecbrussel/webcomponenten/test/maarten/servlet/TestDrieServlet.java
@@ -15,6 +15,13 @@ public class TestDrieServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //req.getSession() creert een nieuwe session
+        // dit kan een bug veroorzaken, je zou in je sessie met een of andere parameter moeten bijhouden of iemand ingelogd is
+        // probeer de volgende stappen:
+        // 1 - surf naar http://localhost:8080/maarten/testdrie
+        // 2 - refresh de pagina f5 of iets dergelijks
+        // 3 - ?
+        // 4 - profit? je krijgt de welcome page te zien zonder ingelogd te zijn.
         if (req.getSession().isNew()){
             req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req,resp);
         } else {
@@ -26,19 +33,25 @@ public class TestDrieServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        // je gebruikt deze string niet
         String nameResult = "";
         String name = req.getParameter("name");
 
 
+        //hier had je HttpSession session = req.getSession(); kunnen schrijven
+
         if (name.equals("")){
+            //1ste keer HttpSession session = req.getSession();
             HttpSession session = req.getSession();
             String message =", gelieve een geldige naam in te voeren!";
+            //je set deze messge maar je gaat ze nooit meer uit je session halen, vanaf nu krijg je dus altijd weer deze tekst te zien
+            // tot je geldig inlogt of je session verloopt
             session.setAttribute("message",message);
             req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req,resp);
 
         }
         else{
+            //2e keer HttpSession session = req.getSession();
             HttpSession session = req.getSession();
             session.setAttribute("name",name);
             req.getRequestDispatcher("/WEB-INF/pages/welcome.jsp").forward(req,resp);
